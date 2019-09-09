7971280307c1f31bd91a51586f3ee4babd3f82e7
steekiller <steekiller@live.com>
2019-09-10 00:30:59 +0200

    testni komit 2

--- src/main/java/com/lanaco/mentor/LanacoAppApplication.java
+++ src/main/java/com/lanaco/mentor/LanacoAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LanacoAppApplication {
 
 	//stefo test git
+	//stefo test 2 git
 	public static void main(String[] args) {
 		SpringApplication.run(LanacoAppApplication.class, args);
 	}
