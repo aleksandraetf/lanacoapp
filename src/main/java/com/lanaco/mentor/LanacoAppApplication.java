a1b75e60f9b46114692301edb430d0dabc3e0a4d
steekiller <steekiller@live.com>
2019-09-09 13:58:46 +0200

    initial commit

---
+++ src/main/java/com/lanaco/mentor/LanacoAppApplication.java
@@ -0,0 +1,13 @@
+package com.lanaco.mentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanacoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanacoAppApplication.class, args);
+	}
+
+}
