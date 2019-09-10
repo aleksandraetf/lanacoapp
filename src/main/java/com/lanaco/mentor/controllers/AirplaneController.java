3b57dc125c8904d2d23f5aacd51430e093d6ebfd
steekiller <steekiller@live.com>
2019-09-10 11:24:11 +0200

    Napravljene klase kontrolera , sa testnim REST mapiranjima , samo za UserController napravljena GET,POST,PUT,DELETE metoda radi testiranja resta :) potrebne izmjene posle zbog integracije sa bazom.

---
+++ src/main/java/com/lanaco/mentor/controllers/AirplaneController.java
@@ -0,0 +1,29 @@
+package com.lanaco.mentor.controllers;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lanaco.mentor.testutil.SteekillerTestUtil;
+
+@RestController
+@RequestMapping(path = "/airplane")
+public class AirplaneController {
+	
+	@GetMapping(path="/all", produces = "application/json")
+	public ResponseEntity<ArrayList<String>> getAll(){
+		try {
+			return new ResponseEntity<ArrayList<String>>(SteekillerTestUtil.readFromFile("test\\airplane.txt"), HttpStatus.OK);
+		}catch(IOException ex) {
+			return null;
+		}
+	}
+	
+	
+	
+}
