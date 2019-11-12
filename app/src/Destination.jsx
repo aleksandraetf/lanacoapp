af488ac0fd6497cfa7c154485788976439e3324f
Aleksandra Milekic <frankfurtxyz1@gmail.com>
2019-11-12 11:24:34 +0100

    frontent

---
+++ app/src/Destination.jsx
@@ -0,0 +1,151 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalBody, InputGroup, InputGroupAddon, Container, Table, Input } from 'reactstrap';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
+class DestinationPage extends Component {
+
+    constructor(props) {
+        super(props);
+
+      /*  checkIfLogged().then(resp => {
+            if (!resp) {
+                this.props.history.push('/')
+            }
+        });
+*/
+      //  this.logOut = this.logOut.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+
+
+        this.state = { destinations: [], showModal: false, message: "", name: "" }
+
+    }
+
+    loadData() {
+        fetch('/api/destination')
+            .then(response => response.json())
+            .then(data => this.setState({ destinations: data }));
+    }
+
+    componentWillMount() {
+        this.loadData();
+    }
+
+    cleanData() {
+        this.setState({ message: "", name: "" });
+    }
+
+    toggle = field => {
+        this.setState(prev => {
+            return { [field]: !prev[field] };
+        });
+    };
+
+  /*  logOut() {
+        fetch('/auth/logout',
+            {
+                method: 'GET',
+                mode: 'cors',
+                headers:
+                {
+                    credentials: 'include'
+                },
+            }
+        ).catch(() => this.props.history.push('/'));
+    }
+*/
+
+
+    handleInputChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+
+    handleSubmit(event) {
+        let dataToSend = {
+            name: this.state.name
+        }
+
+        fetch('/api/destination',
+            {
+                method: 'POST',
+                headers:
+                {
+
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    credentials: 'include'
+                },
+                mode:'cors',
+                //credentials:'include',
+                body: JSON.stringify(dataToSend),
+            }
+        ).then(response => { if (response.status === 202) { this.loadData(); this.cleanData(); this.toggle('showModal'); toast.success("Destination Saved", { position: toast.POSITION_TOP_RIGHT }); } else { this.setState({ message: "Destination not saved! Fields can not be empty and it is not possible to add existing Destination!" }) } });
+    }
+
+    render() {
+        console.log("RENDER:")
+        console.log(this.state);
+        let destinations = [...this.state.destinations];
+        return (
+            <div style={{ backgroundColor: '#923cb5', backgroundImage: `linear-gradient(150deg, #000000 30%, #923cb5 70%)`, margin: 0, height: '100vh', width: '100%', justifyContent: 'center', alignItems: 'center', }}>
+                <ToastContainer autoClose={5000} />
+                <Container>
+                    <Modal
+                        isOpen={this.state.showModal}
+                        toggle={() => this.toggle('showModal')}
+                        className="bg-transparent modal-xl"
+
+                    >
+                        <ModalBody>
+                            <div>
+                                <InputGroup size="sm">
+                                    <InputGroupAddon sm={3} addonType="prepend">
+                                        Destination Name:
+                                    </InputGroupAddon>
+                                    <Input
+                                        type="text" name="name" id="name" value={this.state.name} onChange={this.handleInputChange}
+                                    ></Input>
+                                </InputGroup>
+                              
+                                <p style={{ color: '#923cb5' }}>{this.state.message}</p>
+                                <br></br>
+                                <Button style={{ backgroundColor: "#923cb5" }} onClick={this.handleSubmit}>Add Destination</Button>
+                            </div>
+                        </ModalBody>
+                    </Modal>
+                </Container>
+                <Container>
+                    <Table>
+                        <tbody>
+                            <tr>
+                                <td><h1 style={{ color: "#923cb5" }}>Destination Page</h1></td>
+                             </tr>
+                        </tbody>
+                    </Table>
+                </Container>
+                <Container>
+                    <Button style={{ backgroundColor: "#923cb5" }} onClick={() => this.toggle('showModal')}>Add new Destination</Button>
+                    <Table >
+                        <thead>
+                            <tr><th>ID</th><th>Name</th></tr>
+                        </thead>
+                        <tbody>
+                            {
+                                destinations.map((destination) => {
+                                    return <tr key={destination.id}><td>{destination.id}</td><td>{destination.name}</td></tr>
+                                })
+                            }
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    };
+
+}
+
+export default DestinationPage;
